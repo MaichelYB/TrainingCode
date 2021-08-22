f82a4fe35706e503d177373fc0238f763f9fed5f
Maichel <yunarto609@gmail.com>
2021-08-22 16:55:44 +0700

    add training_3 and training_4

---
+++ training_3.py
@@ -0,0 +1,71 @@
+# Definition for singly-linked list.
+# https://leetcode.com/problems/add-two-numbers-ii/
+# You are given two non-empty linked lists representing two non-negative integers. 
+# The most significant digit comes first and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list.
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Input: l1 = [7,2,4,3], l2 = [5,6,4]
+# Output: [7,8,0,7]
+# 7 2 4 3
+#   5 6 4
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [8,0,7]
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+class Solution:
+  def insert(self, root, item):
+    # change array to linked list
+    temp = ListNode(item)
+    temp.data = item
+    temp.next = root
+    root = temp
+    return root
+  def lengthRecursive(self,head):
+    if head is None:
+      return 0
+    else:
+      return 1 + self.lengthRecursive(head.next)
+  def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    stored = 0
+    root = None
+#   check the lenght of linked list
+    if self.lengthRecursive(l1) > self.lengthRecursive(l2):
+      linked_list_used = l1
+      sec_list = l2
+    else:
+      linked_list_used = l2
+      sec_list = l1
+    len_l1 = self.lengthRecursive(l1)
+    len_l2 = self.lengthRecursive(l2)
+    start_at = abs(len_l1 - len_l2)
+    i = 0
+    results = []
+    while linked_list_used:
+      if i < start_at:
+        results.append(linked_list_used.val)
+      else:
+        results.append(linked_list_used.val + sec_list.val)
+        sec_list = sec_list.next
+      i += 1
+      linked_list_used = linked_list_used.next
+    print(results)
+    for i in reversed(range(len(results))):
+        if stored > 0:
+          results[i] += stored
+          stored = 0
+        if results[i] > 9:
+          results[i] = results[i] - 10
+          stored += 1
+        if i == 0 and stored > 0:
+          bef = [stored]
+          results = bef + results
+    
+    for i in reversed(range(len(results))):
+        root = self.insert(root, results[i])
+    return root
