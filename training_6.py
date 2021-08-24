57ba2ae2fae38539fe5ed16d0094cb125bb1c4b7
Maichel <yunarto609@gmail.com>
2021-08-24 11:58:31 +0700

    finishing training 6, but still got time limit

--- training_6.py
+++ training_6.py
@@ -1,2 +1,21 @@
 # a little bit hard, need extra time to think about the answer
 # https://leetcode.com/problems/longest-palindromic-subsequence/
+
+class Solution:
+  def count_palindrome(self, s, i, j, lookup):
+    if i > j:
+      return 0
+    if i == j:
+      return 1
+    key = (i,j)
+    if key not in lookup:
+      if s[i] == s[j]:
+        lookup['key'] = self.count_palindrome(s, i + 1, j - 1, lookup) + 2
+      else:
+        lookup['key'] = max(self.count_palindrome(s, i+1, j, lookup), self.count_palindrome(s, i, j-1, lookup))
+    return lookup['key']
+  def longestPalindromeSubseq(self, s: str) -> int:
+    i = 0
+    j = len(s)
+    lookup = {}
+    return self.count_palindrome(s, i, j - 1, lookup)
